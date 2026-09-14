089b174e8c19c29d68a5c3330960626459674e00
Akshat Kumar Singh <akshatkusingh@gmail.com>
2026-09-14 11:28:32 +0530

    chore: upgrade lint tooling to ESLint 10

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
+import eslint from "@eslint/js";
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import tseslint from "typescript-eslint";
 
 export default defineConfig([
-  ...nextVitals,
-  ...nextTs,
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
   globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
